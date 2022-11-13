0a23b0cda44fa48f8ce279975c2aa4c6575e48b6
TomasCandido <tomasfcandido@hotmail.com>
2022-11-13 19:58:59 +0000

    Ficheiro Bootstrap no BackEnd e Inicio do Admin Lte

--- municipiomelhor/backend/assets/AppAsset.php
+++ municipiomelhor/backend/assets/AppAsset.php
@@ -12,6 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'css/bootstrap.min.css',
         'css/site.css',
     ];
     public $js = [
